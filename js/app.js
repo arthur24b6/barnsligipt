c18fe55c95ce4bea3e6dd4a8c6a8753d306e684f
Arthur <arthur@24b6.net>
2014-02-13 08:49:45 -0500

    Starting to cleanup and abstract  code

--- js/app.js
+++ js/app.js
@@ -4,11 +4,6 @@
  *
  */
 
-
-var Barnsligipt = {
-  'slideIndex' : 0
-};
-
 /**
  * Application settings.
  */
@@ -19,7 +14,98 @@ var settings = {
 };
 
 
-Barnsligipt.settings = settings;
+function Barnsligipt () {
+
+  this.init = function () {
+   this.getImageListing();
+
+  }
+
+  this.settings =  {
+    contentDirectory: 'images/',
+    baseURL: '',
+    imagesPerPage: 3
+  };
+
+  this.slideIndex = 0;
+  this.images =  {};
+
+  /**
+   * Get the next set of images to display.
+   * @returns images
+   */
+  this.nextSet = function () {
+    var count = this.slideCount();
+    var images = {};
+
+    var stop = this.slideIndex + this.settings.imagesPerPage;
+    if (stop <= count) {
+      // slice on start/stop
+      for (var i = this.slideIndex; i < stop; i++) {
+        images[i] = this.images[i];
+      }
+    }
+    else {
+      var remainder = stop - count;
+      // First slide is the current index to the total number of slides.
+      for (var i; i <= count; i++) {
+        images[i] = this.images[i];
+      }
+      // Second group is the first slide to the remainder.
+      for (var i = 0; i <= remainder; i++) {
+        images[i] = this.images[i];
+      }
+    }
+    return images;
+  };
+
+  this.previousSet = function  () {
+    return images;
+  };
+
+  this.next = function () {
+    var images = this.nextSet();
+
+  }
+
+  this.slideCount = function () {
+    return Object.keys(this.images).length;
+  };
+
+  this.url = function(path) {
+    return this.settings.baseURL + path;
+  };
+
+
+  /**
+   * Get all the posts in the specified directory.
+   *
+   * @TODO handle directories.
+   *
+   * @returns array of post URIs
+   */
+  this.getImageListing = function () {
+    var images = {};
+    // Get the directory listing. Note that the sorting is being done by Apache's
+    // list options. If this isn't supported on the server this won't work.
+    $.ajax({url: url(settings.contentDirectory), async: false})
+      .done(function(data) {
+        var items = $(data).find('a');
+        $(items).each(function(index, value) {
+          // Apache writes the URLs relative to the directory.
+          var uri = url(settings.contentDirectory) + $(this).attr('href');
+          var extension = uri.substr((~-uri.lastIndexOf(".") >>> 0) + 2);
+          // Only support jpeg/jpg file extensions.
+          if (/\.jpe?g?$/i.test ($(this).attr('href'))) {
+            images[index] = uri;
+          }
+        });
+      });
+    this.images = images;
+  }
+
+};
+
 
 
 /**
@@ -39,7 +125,7 @@ function url(path) {
 
 $(document).ready(function() {
 
-
+var slideshow = new Barnsligipt;
 
 /**
  * Get all the posts in the specified directory.
@@ -77,9 +163,8 @@ function getImageListing() {
  * @param array images
  */
 function insertImages(images) {
+  console.log(images);
   $.each(images, function(index, src) {
-    //if (index <= settings.imagesPerPage) {
-      console.log(index);
       var element = $('<a href="' + src +'"></a>');
       var ep = new ExifProcessor(src);
       ep.execute(generateThumb(element, src));
@@ -93,18 +178,8 @@ function insertImages(images) {
         console.log('click');
         return false;
       });
-   // }
-
-   // else {
-      return false;
-   // }
   });
 
-  function buildNavigation(images) {
-    if (images.length > settings.imagesPerPage) {
-
-    }
-  }
 
 
   // Fade the main content area in.
@@ -112,35 +187,19 @@ function insertImages(images) {
 }
 
   // Run the application.
-  Barnsligipt.images = getImageListing();
-  Barnsligipt.slideCount = Object.keys(Barnsligipt.images).length
-  insertImages(Barnsligipt.images);
+
+  slideshow.images = getImageListing();
+  var images = slideshow.nextSet();
+  // Insert the set of images
+  // @TODO this should be the set of images
+  insertImages(images);
 
   // Capture clicks on the navigation.
   $('.navigation').click(function() {
-
+    $('#content').html('');
     if ($(this).hasClass('next')) {
-
-      console.log(Barnsligipt.slideIndex + Barnsligipt.settings.imagesPerPage);
-      console.log(Barnsligipt.images.length); // - Barnsligipt.settings.imagesPerPage);
-      // Advance
-      if ((Barnsligipt.slideIndex + Barnsligipt.settings.imagesPerPage) <= Barnsligipt.slideCount - Barnsligipt.settings.imagesPerPage) {
-        var start = Barnsligipt.slideIndex + Barnsligipt.settings.imagesPerPage;
-        var stop = start + Barnsligipt.settings.imagesPerPage;
-
-        var display = {};
-        for (var i = start; i <= stop; i++) {
-          display[i] = Barnsligipt.images[i];
-          console.log(Barnsligipt.images[i]);
-        }
-
-
-        $('#content').html('');
-        insertImages(display);
-        Barnsligipt.slideIndex = start;
-      }
-
-
+      var images = slideshow.nextSet();
+      insertImages(images);
     }
 
     if ($(this).hasClass('previous')) {
