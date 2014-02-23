6963e8242fcb107b5f5647ac54ffc863bedc6897
Arthur <arthur@24b6.net>
2014-02-22 20:30:07 -0500

    WIP on converting former functionality

--- js/app.js
+++ js/app.js
@@ -1,105 +1,138 @@
-var settings = {
-  contentDirectory: 'images/',
-  baseURL: '/',
-  imagesPerPage: 3,
-  defaultImage: '/404'
-};
-
 /**
- * Utility function to return a path based on the instalation location.
  *
- * @param string path
- *   Create a URL to this path.
- * @returns string
+ * @file
+ * Ember powered slideshow.
  */
-function url(path) {
-  return settings.baseURL + path;
-}
 
 
-/**
- * Utility function to get all images from an Apache directory listing.
- *
- * @returns {Array|getImageListing.listing}
- */
-function getImageListing() {
-  var listing = new Array;
-  Ember.$.ajax({url: url(settings.contentDirectory), async: false})
-    .done(function(data) {
-      var items = Ember.$(data).find('a');
-      Ember.$(items).each(function(index, value) {
-        // Apache writes the URLs relative to the directory.
-        var uri = url(settings.contentDirectory) + Ember.$(this).attr('href');
-        var extension = uri.substr((~-uri.lastIndexOf(".") >>> 0) + 2);
-        // Only support jpeg/jpg file extensions.
-        if (/\.jpe?g?$/i.test (uri)) {
-          listing.push(uri);
-        }
-      });
+App = Ember.Application.create({
+  isViewing: false,
+  slide: 0,
+  page: 0
+});
+
+Ember.$(document).ready(function () {
+  $('.thumbnail, #slide').each(function () {
+    $(this).spin('small', '#999999');
   });
-  return listing;
-}
 
-/**
- * Utility function gets EXIF thumbnail.
- *
- * This fetches the exif thumbnail from the source and fires an event when
- * the image has been loaded.
- *
- * @param string src
- *   The original image path.
- */
-function getThumbnail(src) {
-  var ep = new ExifProcessor(src);
-  ep.execute(function () {
-    // Image has exif thumbnail.
-    if (ep.exifInfo.isValid) {
-      var image = "data:image/jpeg,";
-      image += ep.exifInfo.exifData.toHexString(ep.exifInfo.thumbOffset, ep.exifInfo.thumbLength);
-    }
-    // Image does not have an exif thumbnail.
-    // @TODO add a default image.
-    else {
-      image = settings.defaultImage;
-    }
-    Ember.$.event.trigger({
-      type: 'thumbnailLoaded',
-      'src' : ep.dataSource,
-      'image' : image,
-      'exif' : ep.exifInfo
+  $('.thumbnail img, #slide img').hide();
+
+  $('.thumbnail img, #slide img').on('load', function () {
+    $(this).fadeIn(function() {
+      $(this).parent('div').spin(false);
     });
   });
-}
 
+});
 
-App = Ember.Application.create();
+var slideshow = new Barnsligipt();
+
+var slides = slideshow.images;
+
+var slideIndex = {
+  slide: 0,
+  page: 0
+};
+
 
 App.Router.map(function() {
-  // put your routes here
+  this.resource('about');
+  this.resource('slides', { path: 'slides/:page_id' });
+  this.resource('slide', { path: 'slide/:slide_id' });
 });
+
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    var listing = getImageListing();
-    var images = new Array;
-
-    // Start fetching all the thumbnails.
-    Ember.$.each(listing, function(index, src) {
-      getThumbnail(src);
+    var test = slideshow.images.slice(0, 4);
+    Ember.$.each(test, function(index, slide) {
+      slideshow.getThumbnail(slide);
     });
-
-    // Listen for the thumbnails to complete and update the page.
-    $(document).on('thumbnailLoaded', function(event) {
-      $('img[data-src="'+event.src+'"]').attr('src', event.image).fadeIn(1500);
-    });
-
-    // Return the set of images.
-    return listing;
-   }
+    return test;
+  },
+  actions: {
+    next: function() {
+      this.replaceWith('slides', 1);
+    },
+    previous: function(slide) {
+      this.replaceWith('slides', slideshow.pageCount());
+    }
+  }
 });
 
-App.SlideRoute = Em.Route.extend({
-  model: function() {
-    return App.slide.find();
+
+
+App.SlidesRoute = Ember.Route.extend({
+  model: function(params) {
+    Ember.set(slideIndex, 'page', params.page_id);
+    var slidePage = slideshow.nextSet();
+    Ember.$.each(slidePage, function(index, slide) {
+      slideshow.getThumbnail(slide);
+    });
+    return slidePage;
+  },
+
+  actions: {
+
+    next: function(slides) {
+      var page = Number(slideIndex.page) + 1;
+      if (page >= slideshow.pageCount()) {
+        page = 0;
+      }
+      this.replaceWith('slides', page);
+    },
+
+    previous: function(slides) {
+      var page = slideIndex.page - 1;
+      if (Number(slideIndex.page) <= 0) {
+        page = slideshow.pageCount();
+      }
+      this.replaceWith('slides', page);
+    }
   }
+});
+
+
+
+App.SlideRoute = Ember.Route.extend({
+
+  beforeModel: function(transition) {
+    this.controllerFor('application').set('isViewing', true);
+  },
+
+  model: function(params) {
+    Ember.set(slideIndex, 'slide', params.slide_id);
+    this.slideIndex = Number(params.slide_id);
+    var slide = slides.findBy('id', Number(params.slide_id));
+    return slide;
+  },
+
+  actions: {
+
+    next: function() {
+      var page = Number(slideIndex.slide) + 1;
+      if (page > slideshow.slideCount()) {
+        page = 1;
+      }
+      this.replaceWith('slide', page);
+    },
+
+    previous: function() {
+      var page = Number(slideIndex.slide) - 1;
+      if (page <= 0) {
+        page = slideshow.slideCount();
+      }
+      this.replaceWith('slide', page);
+    },
+
+    close: function() {
+      this.replaceWith('application');
+    }
+  },
+
+  deactivate: function(reason) {
+    this.controllerFor('application').set('isViewing', false);
+  }
+
 });
